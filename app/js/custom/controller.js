93717eb6aff576872371fc064f8a7dd0e7dbed8c
mirabelkoso <mirabelkoso@gmail.com>
2014-12-17 08:48:11 +0100

    looped through the person unordered list using angular

--- app/js/custom/controller.js
+++ app/js/custom/controller.js
@@ -2,11 +2,12 @@
 
  var teamgeeksApp = angular.module('teamgeeksApp', []);
 
- teamgeeksApp.controller('TeamgeeksController', function($scope){
-
-  $scope.hobbies = ["Dami", "Mirabel", "Angular", "NodeJS"];
-  $scope.name = "Mirabel";
-  $scope.age = 35;
-  $scope.goodInAngular = true;
-
- });
+ teamgeeksApp.controller('TeamgeeksController',['$scope', '$http', function($scope,$http){
+    $http.get('js/custom/teamgeeks.json').success(function(data){
+      $scope.persons = data;
+    });
+      // $scope.hobbies = ["Dami", "Mirabel", "Angular", "NodeJS"];
+      // $scope.name = "Mirabel";
+      // $scope.age = 35;
+      // $scope.goodInAngular = true;
+  }]);
