0943c9a670f12755179cdedbbe94170db53e0c17
mirabelkoso <mirabelkoso@gmail.com>
2014-12-17 16:01:45 +0100

    called user data from github's api

--- app/js/custom/controller.js
+++ app/js/custom/controller.js
@@ -1,14 +1,16 @@
 'Use Strict';
 
- var teamgeeksApp = angular.module('teamgeeksApp', []);
-
- teamgeeksApp.controller('TeamgeeksController',['$scope', '$http', function($scope,$http){
-    $http.get('https://api.github.com/users/andela-mekwenugo').success(function(data){
+var teamgeeksApp = angular.module('teamgeeksApp', []);
+var TeamgeeksController = teamgeeksApp.controller;
+teamgeeksApp.controller('TeamgeeksController',['$scope', '$http', function($scope,$http){
+  $scope.searchUser = function(userName) {
+    $http.get("https://api.github.com/users/"+ userName ).success(function(data){
       $scope.gitUsers = data;
-      console.log(data);
+      console.log(data); 
     });
-      // $scope.hobbies = ["Dami", "Mirabel", "Angular", "NodeJS"];
-      // $scope.name = "Mirabel";
-      // $scope.age = 35;
-      // $scope.goodInAngular = true;
-  }]);
+  };
+  // $scope.hobbies = ["Dami", "Mirabel", "Angular", "NodeJS"];
+  // $scope.name = "Mirabel";
+  // $scope.age = 35;
+  // $scope.goodInAngular = true;
+}]);
--- test/unit/controllerspec.js
+++ test/unit/controllerspec.js
@@ -1,85 +1,85 @@
-// testing for array
-describe('TeamgeeksController', function() {
-  beforeEach(module('teamgeeksApp'));
+// // testing for array
+// describe('TeamgeeksController', function() {
+//   beforeEach(module('teamgeeksApp'));
 
-  describe('TeamgeeksControllerHobbies', function() {
-    it('should check if the "hobbies" array exists', inject(function($controller){
+//   describe('TeamgeeksControllerHobbies', function() {
+//     it('should check if the "hobbies" array exists', inject(function($controller){
 
-      var scope = {},
-        controller = $controller('TeamgeeksController', {$scope:scope});
-      expect(scope.hobbies).toBeDefined();
-    }));
+//       var scope = {},
+//         controller = $controller('TeamgeeksController', {$scope:scope});
+//       expect(scope.hobbies).toBeDefined();
+//     }));
     
-    it('should have a maximum length of 4', inject(function($controller){
+//     it('should have a maximum length of 4', inject(function($controller){
 
-      var scope = {},
-        controller = $controller('TeamgeeksController',{$scope:scope});
-      expect(scope.hobbies.length).toBe(4);
-    }));
+//       var scope = {},
+//         controller = $controller('TeamgeeksController',{$scope:scope});
+//       expect(scope.hobbies.length).toBe(4);
+//     }));
 
-    it('should be an array', inject(function($controller) {
+//     it('should be an array', inject(function($controller) {
 
-      var scope = {},
-        controller = $controller('TeamgeeksController',{$scope:scope});
-      expect(Array.isArray(scope.hobbies)).toBe(true);
-    }));
+//       var scope = {},
+//         controller = $controller('TeamgeeksController',{$scope:scope});
+//       expect(Array.isArray(scope.hobbies)).toBe(true);
+//     }));
 
-  });
-
-
-
-  // testing for name
-  describe('TeamgeeksControllerName', function() {
-    it('should check if name exists', inject(function($controller) {
-
-      var scope = {},
-        controller = $controller('TeamgeeksController', {$scope:scope});
-      expect(scope.name).toBeDefined();
-    }));
-
-    it('should check if name is a string', inject(function($controller) {
-
-      var scope = {},
-        controller = $controller('TeamgeeksController', {$scope:scope});
-      expect(typeof scope.name).toEqual('string');
-    }));
-
-  });
+//   });
 
 
-  // testing for age
-  describe('TeamgeeksControllerAge', function() {
-    it('should check if age exists', inject(function($controller) {
 
-      var scope = {},
-        controller = $controller('TeamgeeksController', {$scope:scope});
-      expect(scope.age).toBeDefined();
-    }));
+//   // testing for name
+//   describe('TeamgeeksControllerName', function() {
+//     it('should check if name exists', inject(function($controller) {
 
-    it('should check if age is a number', inject(function($controller) {
+//       var scope = {},
+//         controller = $controller('TeamgeeksController', {$scope:scope});
+//       expect(scope.name).toBeDefined();
+//     }));
 
-      var scope = {},
-        controller = $controller('TeamgeeksController', {$scope:scope});
-      expect(typeof scope.age).toEqual('number');
-    }));
-  });
+//     it('should check if name is a string', inject(function($controller) {
+
+//       var scope = {},
+//         controller = $controller('TeamgeeksController', {$scope:scope});
+//       expect(typeof scope.name).toEqual('string');
+//     }));
+
+//   });
 
 
-  // testing for goodInAngular 
-  describe('TeamgeeksControllerGoodInAngular', function() {
-    it('should check if goodInAngular exists', inject(function($controller) {
-      var scope = {},
-        controller = $controller('TeamgeeksController', {$scope:scope});
-      expect(scope.goodInAngular).toBeDefined();
-    }));
+//   // testing for age
+//   describe('TeamgeeksControllerAge', function() {
+//     it('should check if age exists', inject(function($controller) {
 
-    it('should check if goodInAngular is a boolean', inject(function($controller) {
+//       var scope = {},
+//         controller = $controller('TeamgeeksController', {$scope:scope});
+//       expect(scope.age).toBeDefined();
+//     }));
 
-      var scope = {},
-        controller = $controller('TeamgeeksController', {$scope:scope});
-      expect(typeof scope.goodInAngular).toEqual('boolean');
-    }));
-  });
+//     it('should check if age is a number', inject(function($controller) {
+
+//       var scope = {},
+//         controller = $controller('TeamgeeksController', {$scope:scope});
+//       expect(typeof scope.age).toEqual('number');
+//     }));
+//   });
+
+
+//   // testing for goodInAngular 
+//   describe('TeamgeeksControllerGoodInAngular', function() {
+//     it('should check if goodInAngular exists', inject(function($controller) {
+//       var scope = {},
+//         controller = $controller('TeamgeeksController', {$scope:scope});
+//       expect(scope.goodInAngular).toBeDefined();
+//     }));
+
+//     it('should check if goodInAngular is a boolean', inject(function($controller) {
+
+//       var scope = {},
+//         controller = $controller('TeamgeeksController', {$scope:scope});
+//       expect(typeof scope.goodInAngular).toEqual('boolean');
+//     }));
+//   });
 
 
   //TESTING FOR HTTP REQUEST
@@ -92,7 +92,8 @@ describe('TeamgeeksController', function() {
 
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
       $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('https://api.github.com/users/andela-mekwenugo').respond({ "login": "octocat", "type": "User", "followers": 20 });
+      // $httpBackend.expectGET('https://api.github.com/users/'+userName).respond({ "login": "octocat", "type": "User", "followers": 20 });
+      $httpBackend.expectGET('https://api.github.com/users/'+userName).respond({ "login": "octocat", "type": "User", "followers": 20 });
 
       scope = $rootScope.$new();
       ctrl = $controller('TeamgeeksController', {$scope: scope});
