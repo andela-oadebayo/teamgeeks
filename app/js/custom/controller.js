10104ca9bdb3088ff26b522c816990cfe2957b1e
Adebayo Oluwadamilola <bola3624@gmail.com>
2014-12-17 12:03:39 +0100

    Worked on gitHub Api

--- app/js/custom/controller.js
+++ app/js/custom/controller.js
@@ -3,8 +3,9 @@
  var teamgeeksApp = angular.module('teamgeeksApp', []);
 
  teamgeeksApp.controller('TeamgeeksController',['$scope', '$http', function($scope,$http){
-    $http.get('js/custom/teamgeeks.json').success(function(data){
-      $scope.persons = data;
+    $http.get('https://api.github.com/users/andela-mekwenugo').success(function(data){
+      $scope.gitUsers = data;
+      console.log(data);
     });
       // $scope.hobbies = ["Dami", "Mirabel", "Angular", "NodeJS"];
       // $scope.name = "Mirabel";
